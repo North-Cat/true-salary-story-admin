268f6867f30e6e4e9e77e3e447ba1976d9a02875
May.Yang <may.yang@hy-tech.com.tw>
2023-05-03 12:20:24 +0800

    fix: 調整登入驗證

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,7 @@ import router from './router';
 import { useAdminStore } from '@/stores/admin';
 
 router.beforeEach((to, from, next) => {
-  // 取用 pinia 一定要寫在 router.beforeEach 裡面才有用
-  const adminStore = useAdminStore();
-  const isLogin = adminStore.isLogin; // 是否有登入
+  const isLogin = localStorage.getItem('auth_token'); // 是否有登入
   if (to.name === 'login' && isLogin){
     // 如果已經登入就不用再進登入頁，轉到頁面
     next('/');
