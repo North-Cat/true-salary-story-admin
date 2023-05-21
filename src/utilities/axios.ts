09c69a14c6ba7f281a406b2fe85db48ecaef5d5f
May.Yang <may.yang@hy-tech.com.tw>
2023-05-21 17:12:53 +0800

    fix: 修正 ts 錯誤

--- src/utilities/axios.ts
+++ src/utilities/axios.ts
@@ -29,31 +29,22 @@ axiosInstance.interceptors.response.use(
   },
   function (error) {
     // 呼叫後端失敗，若為權限失敗要跳轉頁面，其餘 errorCallBack
-    if (checkAuthorized(error)) {
-      return Promise.reject(error.response.data)
+    if (error && error.response && error.response.status) {
+      if (401 == error.response.status){
+        let message = error.response.data ? error.response.data.message : undefined;
+        showError("權限錯誤", message ? message : "" );
+  
+        // 清空登入資訊
+        removeCookie('token');
+        removeCookie('account');
+        // 跳轉至 login
+        router.push('/login');
+
+      }else {
+        return Promise.reject(error.response.data)
+      }
     }
   }
 )
-
-
-// 判斷權限驗證是否通過
-function checkAuthorized(error : AxiosError) {
-  if (error && error.response && error.response.status) {
-    // token 驗證失敗回傳 401，導頁至權限失敗頁面
-    if (401 == error.response.status) {
-      let message = error.response.data ? error.response.data.message : undefined;
-      showError("權限錯誤", message ? message : "" );
-
-      // 清空登入資訊
-      removeCookie('token');
-      removeCookie('account');
-      // 跳轉至 login
-      router.push('/login');
-
-      return false;
-    }
-  }
-	return true;
-}
 
 export default axiosInstance
